d4af109218ab66d637c46446f036d181c63aa5b0
wbsuser <sunwb_mai@163.com>
2020-03-31 22:33:21 +0800

    完成图的链式前向星构建

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,9 +1,7 @@
 package graph;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.logging.Logger;
 
 class Edge {
@@ -19,9 +17,13 @@ class Edge {
 public class Graph {
     final int MaxEdges = 5100;
     public Edge[] edges = new Edge[MaxEdges];
-    private int cnt;
+    public int cnt;
+    boolean[] visitedEdges;
+    public List<LinkedHashSet<Integer>> path;
     private String inputFileName;
     private Logger logger;
+    final private int MinLen = 3;  //环的最小长度
+    final private int MaxLen = 7;  //环的最大长度
     Map<Integer, Integer> head = new HashMap<>();
     public Graph(String inputFileName){
         logger = Logger.getLogger("Graph");
@@ -32,6 +34,9 @@ public class Graph {
         } catch (IOException e) {
             logger.info("Fail: loadFile...");
         }
+
+        visitedEdges = new boolean[cnt];
+        path = new LinkedList<>();
     }
 
     //读取文件，按照链式前向星的方法为图添加边
@@ -56,8 +61,82 @@ public class Graph {
         head.put(u, cnt++);
     }
 
+    public void findLoop(){
+        for (int node : head.keySet()) {
+            LinkedHashSet<Integer> nodeList = new LinkedHashSet<>();
+            nodeList.add(node);
+            dfs(node, node, nodeList);
+        }
+        Collections.sort(path, new Comparator<LinkedHashSet<Integer>>() {
+            @Override
+            public int compare(LinkedHashSet<Integer> o1, LinkedHashSet<Integer> o2) {
+                if (o1.size() != o2.size()) return o1.size() - o2.size();
+                else {
+                    Iterator<Integer> it1, it2;
+                    it1 = o1.iterator();
+                    it2 = o2.iterator();
+                    while (it1.hasNext() && it2.hasNext()) {
+                        int n = it1.next() - it2.next();
+                        if (n != 0) return n;
+                    }
+                    return 0;
+                }
+            }
+        });
+    }
+    //dfs寻找长度为3~7的环路
+    public void dfs(int root, int node, LinkedHashSet<Integer> nodeList) {
+        if (!head.containsKey(node)) return;
+        int index = head.get(node);
+        if (visitedEdges[index]) return ;
+        while (index != -1) {
+            Edge e = edges[index];
+            visitedEdges[index] = true;
+            if (root == e.end) {
+                if (nodeList.size()<=MaxLen && nodeList.size()>=MinLen) path.add(nodeList);
+                return;
+            } else if (nodeList.contains(e.end)) {  //相当于链表带环但环的入口不在头节点
+                LinkedHashSet<Integer> list = new LinkedHashSet<>();
+                boolean flag = false;
+                for (int x : nodeList) {
+                    if (x == e.end) {
+                        flag = true;
+                    }
+                    if (flag) {
+                        list.add(x);
+                    }
+                }
+                if (list.size()<=MaxLen && list.size()>=MinLen) path.add(list);
+            }
+            nodeList.add(e.end);
+            dfs(root, e.end, nodeList);
+            index = e.next;
+        }
+    }
     public static void main(String[] args) {
-        Graph graph = new Graph("src/data/test_data.txt");
+        String inputFile = "src/data/test_data.txt";
+        String outputFile = "src/data/answer.txt";
+        Graph graph = new Graph(inputFile);
+//        System.out.println(graph.cnt);
+        graph.findLoop();
+        try {
+            File file = new File(outputFile);
+
+            if (!file.exists())
+                file.createNewFile();
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bufferedWriter = new BufferedWriter(fw);
+            bufferedWriter.write(String.valueOf(graph.path.size()));
+            bufferedWriter.newLine();
+            for (LinkedHashSet<Integer> list : graph.path) {
+                bufferedWriter.write(list.toString());
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.close();
+        } catch (IOException e) {
+            graph.logger.info("Fail: create file!");
+        }
+        System.out.println(graph.path.size());
 
     }
 }
